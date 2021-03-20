e6b1bb990122068b33c638c0dc01eed0acb14955
UnGast <zadrian@web.de>
2021-03-20 09:18:17 +0100

    implement cube mesh faces

--- Sources/swift-vulkan-test/CubeMesh.swift
+++ Sources/swift-vulkan-test/CubeMesh.swift
@@ -2,16 +2,49 @@ import GfxMath
 
 public class CubeMesh: Mesh {
   public var position: FVec3 = .zero
+  public var modelTransformation: FMat4 = .identity
 
   public var vertices: [Vertex] {
     [
-      Vertex(position: Position3(x: 0, y: 1, z: 1), color: Color(r: 1, g: 1, b: 0), texCoord: Position2(x: 0, y: 0)),
-      Vertex(position: Position3(x: 1, y: 1, z: 1), color: Color(r: 1, g: 1, b: 0), texCoord: Position2(x: 0, y: 0)),
-      Vertex(position: Position3(x: 1, y: 0, z: 1), color: Color(r: 1, g: 1, b: 0), texCoord: Position2(x: 0, y: 0)),
+      // 0, top front left
+      Vertex(position: FVec3(x: -0.5, y: 0.5, z: -0.5), color: .yellow, texCoord: FVec2(x: 0, y: 0)),
+      // 1, top front right
+      Vertex(position: FVec3(x: 0.5, y: 0.5, z: -0.5), color: .yellow, texCoord: FVec2(x: 0, y: 0)),
+      // 2, bottom front right
+      Vertex(position: FVec3(x: 0.5, y: -0.5, z: -0.5), color: .yellow, texCoord: FVec2(x: 0, y: 0)),
+      // 3, bottom front left
+      Vertex(position: FVec3(x: -0.5, y: -0.5, z: -0.5), color: .yellow, texCoord: FVec2(x: 0, y: 0)),
+      // 4, top back left
+      Vertex(position: FVec3(x: -0.5, y: 0.5, z: 0.5), color: .yellow, texCoord: FVec2(x: 0, y: 0)),
+      // 5, top back right
+      Vertex(position: FVec3(x: 0.5, y: 0.5, z: 0.5), color: .yellow, texCoord: FVec2(x: 0, y: 0)),
+      // 6, bottom back left
+      Vertex(position: FVec3(x: -0.5, y: -0.5, z: 0.5), color: .yellow, texCoord: FVec2(x: 0, y: 0)),
+      // 7, bottom back right
+      Vertex(position: FVec3(x: 0.5, y: -0.5, z: 0.5), color: .yellow, texCoord: FVec2(x: 0, y: 0)),
     ]
   }
   
   public var indices: [UInt32] {
-    [0, 1, 2]
+    [
+      // front 
+      0, 1, 2,
+      0, 2, 3,
+      // right
+      1, 5, 7,
+      1, 7, 2,
+      // left
+      4, 0, 3,
+      4, 3, 6,
+      // bottom
+      3, 2, 7,
+      3, 7, 6,
+      // back
+      5, 4, 6,
+      5, 6, 7,
+      // top
+      4, 5, 1,
+      4, 1, 0
+    ]
   }
 }
